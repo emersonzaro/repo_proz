7981c1eb3f9c8e39c860ea6cd7e5599ca791e382
emersonzaro <emersonzaro@gmail.com>
2023-10-09 15:47:10 -0300

    Fim módulo Javascript I

---
+++ 05 - Javascript 1/seletores-dom/script.js
@@ -0,0 +1,19 @@
+const textos = document.getElementsByClassName("texto-simples");
+
+
+
+ 
+
+console.log(textos[0]);
+
+
+
+
+
+const segundoTitulo = document.querySelector("div h2");
+
+
+
+ 
+
+console.log(segundoTitulo);
